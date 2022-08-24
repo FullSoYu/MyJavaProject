27dc7869fb3fabe4223f0f96acf22c6804cec93b
FullSoYu <hey6410@naver.com>
2022-08-24 19:27:51 +0900

    Initial commit

---
+++ src/main/java/com/example/MyJavaProject/Main.java
@@ -0,0 +1,15 @@
+package com.example.MyJavaProject;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        전사 a전사 = new 전사();
+        a전사.공격();
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+    }
+}
+
